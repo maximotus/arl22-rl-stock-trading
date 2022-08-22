995a7494645ad21f402edc658c4f73c83772773d
jflxb <jonas@blenninger.de>
2022-08-22 19:16:35 +0200

    added social sentiment to data

--- rltrading/data/fundamental.py
+++ rltrading/data/fundamental.py
@@ -1,29 +1,113 @@
-# import requests
-# from bs4 import BeautifulSoup
-# import pandas as pd
+import os
+from typing import List
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
-
-# base_path = "https://traderfox.de/aktien/{0}/fundamental"
-
-# print(base_path.format('40678-apple-inc'))
-# page = requests.get(base_path)
-# soup = BeautifulSoup(page.text, 'lxml')
-# print(soup)
-
-# table = soup.find('table')
-# print(table)
-
+import numpy as np
 import pandas as pd
 import finnhub as fh
 
 
-finnhub_client = fh.Client(api_key="cc0uvnaad3ifk6takpvg")
-print(finnhub_client.stock_social_sentiment("GME"))
+def get_social_sentiment(
+    symbol: str, _from: datetime, to: datetime, lookback: timedelta
+) -> pd.DataFrame:
+    load_dotenv()
+    finnhub_api_key = os.getenv("FINNHUB_API_KEY", None)
+    fh_client = fh.Client(api_key=finnhub_api_key)
+
+    from_ = (_from - lookback).strftime("%Y-%m-%d")
+    to_ = to.strftime("%Y-%m-%d")
+    social_sentiment = fh_client.stock_social_sentiment(symbol, _from=from_, to=to_)
+
+    sentiment_data_frames = []
+    sentiment_platforms = ["reddit", "twitter"]
+    for sentiment_platform in sentiment_platforms:
+        sentiment_df = pd.DataFrame(social_sentiment[sentiment_platform])
+        if len(sentiment_df) == 0:
+            continue
+        sentiment_df["atTime"] = (
+            pd.to_datetime(sentiment_df["atTime"]).astype(np.int64) // 10**9
+        )
+        sentiment_df = sentiment_df.rename(
+            columns={
+                "atTime": "time",
+                "positiveScore": "postitive_score",
+                "negativeScore": "negative_score",
+                "negativeMention": "negative_mention",
+                "positiveMention": "positive_mention",
+            }
+        )
+        sentiment_df = sentiment_df.rename(
+            columns={
+                column: f"{sentiment_platform}_{column}"
+                for column in sentiment_df.columns
+                if column != "time"
+            }
+        )
+        sentiment_data_frames.append(sentiment_df)
+
+    if len(sentiment_data_frames) == 2:
+        sentiment_df = sentiment_data_frames[0].merge(
+            sentiment_data_frames[1], how="outer"
+        )
+        sentiment_df = sentiment_df.fillna(value=0.0)
+        sentiment_df = sentiment_df.sort_values(["time"])
+    else:
+        sentiment_df = pd.DataFrame()
+    return sentiment_df
 
 
-def get_social_sentiment(symbol: str, fh_client: fh.Client) -> pd.DataFrame:
-    pass
+def get_social_sentiment_alternatives(
+    symbol: str, days: List[int], lookback: timedelta
+) -> pd.DataFrame:
+    load_dotenv()
+    finnhub_api_key = os.getenv("FINNHUB_API_KEY", None)
+    fh_client = fh.Client(api_key=finnhub_api_key)
+
+    # from_ = (_from - lookback).strftime("%Y-%m-%d")
+    # to_ = to.strftime("%Y-%m-%d")
+    all_sentiments = []
+    for day in days:
+        # from_ = datetime.fromtimestamp(day).strftime("%Y-%m-%d")
+        social_sentiment = fh_client.stock_social_sentiment(symbol, _from=day)
+
+        sentiment_data_frames = []
+        sentiment_platforms = ["reddit", "twitter"]
+        for sentiment_platform in sentiment_platforms:
+            sentiment_df = pd.DataFrame(social_sentiment[sentiment_platform])
+            if len(sentiment_df) == 0:
+                continue
+            sentiment_df["atTime"] = (
+                pd.to_datetime(sentiment_df["atTime"]).astype(np.int64) // 10**9
+            )
+            sentiment_df = sentiment_df.rename(
+                columns={
+                    "atTime": "time",
+                    "positiveScore": "postitive_score",
+                    "negativeScore": "negative_score",
+                    "negativeMention": "negative_mention",
+                    "positiveMention": "positive_mention",
+                }
+            )
+            sentiment_df = sentiment_df.rename(
+                columns={
+                    column: f"{sentiment_platform}_{column}"
+                    for column in sentiment_df.columns
+                    if column != "time"
+                }
+            )
+            sentiment_data_frames.append(sentiment_df)
+
+        if len(sentiment_data_frames) == 2:
+            sentiment_df = sentiment_data_frames[0].merge(
+                sentiment_data_frames[1], how="outer"
+            )
+            sentiment_df = sentiment_df.fillna(value=0.0)
+            all_sentiments.append(sentiment_df)
+
+    sentiments_df = pd.concat(all_sentiments)
+    return sentiments_df
 
 
 if __name__ == "__main__":
-    pass
+    get_social_sentiment("AAPL", 1660892400, 1660892800)
--- rltrading/data/handler.py
+++ rltrading/data/handler.py
@@ -1,126 +1,83 @@
-from datetime import datetime
-from typing import List, Tuple
-
-import pytz
+from typing import List
+from datetime import datetime, timedelta
+import calendar
 import numpy as np
 import pandas as pd
 
-import MetaTrader5 as mt5
-
-from tqdm import tqdm
-
-
-# _DEFAULT_FROM = datetime(
-#     year=2018,
-#     month=1,
-#     day=1,
-#     hour=0,
-#     minute=0,
-#     second=0,
-# )
-_DEFAULT_FROM = datetime(
-    year=2022,
-    month=8,
-    day=17,
-    hour=0,
-    minute=0,
-    second=0,
+from rltrading.data.fundamental import (
+    get_social_sentiment,
 )
-
-_DEFAULT_TO = datetime(
-    year=2022,
-    month=12,
-    day=31,
-    hour=23,
-    minute=59,
-    second=59,
-)
+from rltrading.data.meta_trader import get_stock_data
 
 
 def get_data(
-    symbol: str, from_: datetime = _DEFAULT_FROM, to: datetime = _DEFAULT_TO
+    symbol: str, _from: datetime, to: datetime, lookback: timedelta
 ) -> pd.DataFrame:
-    __init_mt5()
-
-    trading_days = __get_trading_days(symbol, from_, to)
-
-    ohlc_df = __get_ohlc_data(symbol, trading_days)
-    times = ohlc_df["time"].tolist()
-
-    tick_lookback = __get_tick_lookback(times)
-    ticks_df = __get_tick_data(symbol, tick_lookback, times)
-
-    merged = ohlc_df.merge(ticks_df)
-    return merged
+    meta_trader_data = get_stock_data(symbol, _from, to)
+    social_sentiment = get_social_sentiment(symbol, _from, to, lookback=lookback)
+    times = meta_trader_data["time"].values
+    social_sentiment = __aggregate_social_sentiment(social_sentiment, times, lookback)
+    total = meta_trader_data.merge(social_sentiment, how="left")
+    total = total.fillna(value=0.0)
+    total = total.sort_values(["time"])
+    return total
 
 
-def __get_ohlc_data(
-    symbol: str, trading_days: List[Tuple[datetime, datetime]]
+def __aggregate_social_sentiment(
+    sentiments: pd.DataFrame, times: List[int], lookback: timedelta
 ) -> pd.DataFrame:
-    ohlc_data = []
-    ohlc_columns = []
-    for start, end in tqdm(trading_days):
-        start = pytz.utc.localize(start)
-        end = pytz.utc.localize(end)
-
-        rates = mt5.copy_rates_range(symbol, mt5.TIMEFRAME_M1, start, end)
-
-        ohlc_columns = pd.DataFrame(rates).columns.tolist()
-        ohlc_data.extend(list(map(list, rates)))
-
-    ohlc_df = pd.DataFrame(ohlc_data, columns=ohlc_columns).drop_duplicates()
-    return ohlc_df
-
-
-def __get_tick_data(symbol: str, tick_lookback: int, stamps: int) -> pd.DataFrame:
-    data_frames = []
-    for time in tqdm(stamps):
-        ticks = mt5.copy_ticks_range(
-            symbol, time - tick_lookback, time, mt5.COPY_TICKS_ALL
-        )
-        ticks_df = pd.DataFrame(ticks)
-        ticks_df["time"] = time
-        data_frames.append(ticks_df)
-
-    ticks_df = pd.concat(data_frames)
-    ticks_df = ticks_df.groupby(["time"]).describe().reset_index()
-    ticks_df.columns = [
-        "_".join(col) if col[1] != "" else col[0] for col in ticks_df.columns.values
+    sentiments = __presort_sentiments(sentiments, times, lookback)
+    intermediates = []
+    for upper_bound in times:
+        lower_bound = __substract_time(upper_bound, lookback)
+        intermediate = sentiments.loc[
+            (sentiments["time"] >= lower_bound) & (sentiments["time"] <= upper_bound)
+        ].copy(deep=True)
+        if len(intermediate) == 0:
+            continue
+        intermediate["time"] = upper_bound
+        intermediates.append(intermediate)
+    total_sentiments = pd.concat(intermediates)
+    total_sentiments = total_sentiments.groupby(["time"]).describe().reset_index()
+    total_sentiments.columns = [
+        "_".join(col) if col[1] != "" else col[0] for col in total_sentiments.columns.values
     ]
-    return ticks_df
+    return total_sentiments
 
 
-def __get_tick_lookback(times) -> int:
-    return int(np.argmax(np.bincount(np.diff(times))))
+def __presort_sentiments(
+    sentiments: pd.DataFrame, times: List[int], lookback: timedelta
+) -> pd.DataFrame:
+    total_lower_bound = __substract_time(np.min(times), lookback)
+    bigger_than_min = sentiments["time"] >= total_lower_bound
+    smaller_than_max = sentiments["time"] <= np.max(times)
+    sentiments = sentiments.loc[bigger_than_min & smaller_than_max]
+    return sentiments
 
 
-def __get_trading_days(
-    symbol, from_: datetime, to: datetime
-) -> List[Tuple[datetime, datetime]]:
-    trading_days = __get_raw_trading_days(symbol, from_, to)
-
-    start_end = []
-    for day in trading_days:
-        dt = datetime.fromtimestamp(day)
-        start = datetime(dt.year, dt.month, dt.day, 0, 0, 1)
-        end = datetime(dt.year, dt.month, dt.day, 23, 59, 59)
-        start_end.append((start, end))
-    return start_end
-
-
-def __get_raw_trading_days(symbol: str, from_: datetime, to: datetime) -> List[int]:
-    from_ = pytz.utc.localize(from_)
-    to = pytz.utc.localize(to)
-    rates = mt5.copy_rates_range(symbol, mt5.TIMEFRAME_D1, from_, to)
-    trading_days = rates["time"]
-    sorted(trading_days)
-    return trading_days
-
-
-def __init_mt5():
-    if not mt5.initialize():
-        raise (RuntimeError(f"Failed to initialize MT5: {mt5.last_error()}"))
+def __substract_time(timestamp: int, lookback: timedelta) -> int:
+    return calendar.timegm(
+        (datetime.fromtimestamp(timestamp) - lookback).utctimetuple()
+    )
 
 
 if __name__ == "__main__":
-    get_data("NVDA")
+    _from = datetime(
+        year=2022,
+        month=8,
+        day=10,
+        hour=0,
+        minute=0,
+        second=0,
+    )
+
+    to = datetime(
+        year=2022,
+        month=12,
+        day=31,
+        hour=23,
+        minute=59,
+        second=59,
+    )
+    lookback = timedelta(days=1)
+    get_data("GME", _from, to, lookback)
