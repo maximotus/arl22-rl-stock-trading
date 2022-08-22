d0e4ca8725b7675b45765e26ec830996ef3422a0
Maximotus <dP=+Zl$2?P+7Z~#Ax-+3-$=1aOZ0^2([VUr$mA5VME[w0*cJQpQQ5kAC7BG&f,PUS.#pSSP21jP.q-g}Gs,6/F\{?\er{TnIF>
2022-08-22 17:58:01 +0200

    reformatted the file

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ def main():
     log_fmt = configuration.get("logger").get("format")
     experiment_path = create_experiment_dir(configuration_file, configuration.get("experiment_path"),
                                             configuration.get("model").get("pretrained_path"), mode)
+
     logger = setup_logger(experiment_path, log_lvl, log_fmt)
     logger.info("Successfully read the given configuration file, created experiment directory and set up logger.")
     logger.info('Starting experiment in mode ' + mode + ' using configuration ' + configuration_file)
