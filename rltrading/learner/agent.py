8de83b1c1fc43a3399a4c7a5aa10369f45a02237
Maximotus <dP=+Zl$2?P+7Z~#Ax-+3-$=1aOZ0^2([VUr$mA5VME[w0*cJQpQQ5kAC7BG&f,PUS.#pSSP21jP.q-g}Gs,6/F\{?\er{TnIF>
2022-08-24 09:53:21 +0200

    fixed the code regarding semantics of stable-baselines

--- rltrading/learner/agent.py
+++ rltrading/learner/agent.py
@@ -13,15 +13,17 @@ class Agent:
     ):
         self.env = gym.make(gym_id)
         self.model = rl_models[rl_model_id](policy_id, self.env, verbose=verbose)
-
-        # use a model pretrained for #epochs timesteps
-        self.model.learn(total_timesteps=epochs, log_interval=log_interval)
+        self.epochs = epochs
+        self.log_interval = log_interval
+        self.save_path = save_path
 
     def learn(self):
         logger.info("Start learning using model " + self.model)
 
-        # TODO the following is just copied from https://stable-baselines3.readthedocs.io/en/master/modules/dqn.html
-        # as an example how to use this function
+        self.model.learn(total_timesteps=self.epochs, log_interval=self.log_interval)
+        self.model.save(self.save_path)
+
+    def apply(self):
         obs = self.env.reset()
         while True:
             action, _states = self.model.predict(obs, deterministic=True)
