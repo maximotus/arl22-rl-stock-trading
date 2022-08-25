35e9e14ddc443d3483511de557a7b077a4916a95
Maximotus <dP=+Zl$2?P+7Z~#Ax-+3-$=1aOZ0^2([VUr$mA5VME[w0*cJQpQQ5kAC7BG&f,PUS.#pSSP21jP.q-g}Gs,6/F\{?\er{TnIF>
2022-08-25 09:08:01 +0200

    added device_name parameter (in ["cuda", "cpu", "auto"])

--- rltrading/learner/agent.py
+++ rltrading/learner/agent.py
@@ -1,11 +1,13 @@
 import gym
 import logging
+import torch
 
 from stable_baselines3 import DQN, PPO, A2C
 
 logger = logging.getLogger("root")
 rl_models = {"PPO": PPO, "DQN": DQN, "A2C": A2C}
 policy_ids = ["MlpPolicy", "CnnPolicy", "MultiInputPolicy"]
+devices = ["cpu", "cuda", "auto"]
 
 
 class Agent:
@@ -18,19 +20,29 @@ class Agent:
         epochs: int,
         log_interval: int,
         save_path: str,
+        device_name: str,
     ):
         # validity checking
         if rl_model_id not in rl_models.keys():
             raise ValueError(f"Unknown RL-Model: {rl_model_id}")
         if policy_id not in policy_ids:
             raise ValueError(f"Unknown policy id: {policy_id}")
+        if device_name not in devices:
+            raise ValueError(f"Unknown device name: {device_name}")
 
         # initialization
         self.gym_env = gym_env
-        self.model = rl_models[rl_model_id](policy_id, self.gym_env, verbose=verbose)
         self.epochs = epochs
         self.log_interval = log_interval
         self.save_path = save_path
+
+        device = (
+            torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            if device_name == "auto"
+            else torch.device(device_name)
+        )
+
+        self.model = rl_models[rl_model_id](policy_id, self.gym_env, verbose=verbose, device=device)
 
     def learn(self):
         self.model.learn(total_timesteps=self.epochs, log_interval=self.log_interval)
@@ -49,6 +61,6 @@ class Agent:
 # for testing
 if __name__ == "__main__":
     env = gym.make("CartPole-v0")
-    a = Agent(env, "DQN", "MlpPolicy", 0, 1000, 100, "./result_dqn_cartpole")
+    a = Agent(env, "DQN", "MlpPolicy", 0, 1000, 100, "./result_dqn_cartpole", "auto")
     a.learn()
     a.apply()
