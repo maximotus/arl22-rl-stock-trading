c0cbd1be79dd557272938a8b6e3627187558fe71
JustARand0m <korbinian.federholzner@t-online.de>
2022-08-29 13:24:18 +0000

    very simple gym environment

--- rltrading/gym/environment.py
+++ rltrading/gym/environment.py
@@ -8,8 +8,8 @@ from rltrading.data.data import Data
 
 
 class Environment(gym.Env):
-    BUY_ACTION = 1
-    SELL_ACTION = -1
+    BUY_ACTION = 2
+    SELL_ACTION = 1
     HOLD_ACTION = 0
 
     """
@@ -17,80 +17,66 @@ class Environment(gym.Env):
     """
 
     def __init__(self: "Environment", shares: int, money: float, data: Data):
+        self.amount = 1
+        self.active_position = False
+        self.shares = 0
+        self.money = money
+        self.balance = money
+        self.last_price = 0
+
         self.data = data
-        self.state = [0, 0, 0, 0, 0, 0, 0]  # TODO
-        self.money = np.asarray([money], dtype=float)
-        self.action = np.asarray([], dtype=float)
-        self.shares = shares
-        self.portfolio_value = 0
         self.time = 0
-        self.action_space = spaces.Box(
-            low=np.array([-1]), high=np.array([1]), dtype=float
-        )
+        self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(
-            low=-inf, high=inf, shape=(len(self.state), 1), dtype=float
+            low=-inf, high=inf, shape=(1,), dtype=np.float32
         )
+
+    def _get_obs(self):
+        curr_observation = self.data.item(self.time)
+        curr_close = curr_observation.value("close")
+        return np.array([curr_close])
 
     def reset(self):
         self.time = 0
-        self.money = np.asarray([0])
         self.shares = 0
-        self.portfolio_value = 0
+        self.balance = self.money
+        self.active_position = False
+        self.last_price = 0
+        return self._get_obs()
 
-    def step(self, action: float):
-        curr_money = self.money[self.time]
+    def step(self, action: int):
         curr_observation = self.data.item(self.time)
         curr_close = curr_observation.value("close")
+        reward = 0
 
-        # Buy
-        if action > self.HOLD_ACTION:
-            amount_of_shares = int(action * curr_money / curr_close)
-            costs = curr_close * amount_of_shares
-            self.money = np.append(self.money, (curr_money - costs))
-            self.shares += amount_of_shares
-            self.action = np.append(self.action, self.BUY_ACTION)
+        if self.active_position == False:
+            if action == self.BUY_ACTION:
+                self.balance -= curr_close * self.amount
+                self.shares += self.amount
+                self.last_price = curr_close
+                self.active_position = True
 
-        # Sell
-        if action < self.HOLD_ACTION:
-            amount_of_shares = int(action * self.portfolio_value / curr_close) * -1
-            gain = curr_close * amount_of_shares
-            if amount_of_shares <= self.shares:
-                self.money.loc[len(self.money.index)] = curr_money + gain
-                self.money = np.append(self.money, (curr_money + gain))
-                self.shares -= amount_of_shares
-                self.action = np.append(self.action, self.SELL_ACTION)
+            if action == self.SELL_ACTION:
+                self.balance -= curr_close * self.amount
+                self.shares -= self.amount
+                self.last_price = curr_close
+                self.active_position = True
+        else:
+            if action == self.BUY_ACTION and self.shares < 0:
+                reward = (-self.shares) * self.last_price - curr_close * self.shares
+                self.shares = 0
+                self.active_position = False
+            if action == self.SELL_ACTION and self.shares > 0:
+                reward = curr_close * self.shares - self.shares * self.last_price  
+                self.shares = 0
+                self.active_position = False
 
-        # Hold
-        if action == self.HOLD_ACTION:
-            self.action = np.append(self.action, self.HOLD_ACTION)
-
+                
         self.time += 1
-        next_money = self.money[self.time]
-        next_observation = self.data.item(self.time)
-
-        self.portfolio_value = self.shares * next_observation.value("close")
-
-        next_money = self.money[self.time]
-        next_observation = self.data.item(self.time)
-
-        self.state = [
-            *next_observation.all(),
-            self.shares,
-            self.portfolio_value,
-            next_money,
-            self.action,
-        ]
-
-        info = {"Round: ", self.time}
-        self.reward = (self.money[self.time] - self.money[self.time - 1]) / self.money[
-            self.time - 1
-        ]
-
+        info = {}
         done = self.data.has_next(self.time)
-        return self.state, self.reward, done, info
+        return self._get_obs(), reward, done, info
 
     def render(self):
         pass
 
-    def getObsStateSize(self):
-        return len(self.state)
