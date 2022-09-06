2b004397b4a0565d20e6300bb79c0a9d62caf599
Maximotus <dP=+Zl$2?P+7Z~#Ax-+3-$=1aOZ0^2([VUr$mA5VME[w0*cJQpQQ5kAC7BG&f,PUS.#pSSP21jP.q-g}Gs,6/F\{?\er{TnIF>
2022-09-06 11:59:05 +0200

    reformatted using black .

--- rltrading/learn/result_plotting.py
+++ rltrading/learn/result_plotting.py
@@ -3,10 +3,11 @@ import matplotlib.gridspec as gridspec
 import numpy as np
 from rltrading.gym.environment import Actions, Positions
 
+
 def plot_results(result_memory):
     time = range(len(result_memory))
     gs = gridspec.GridSpec(2, 2, wspace=0.3, hspace=0.3)
-    #fig, axes = plt.subplots(2, 2, figsize=(20,8))
+    # fig, axes = plt.subplots(2, 2, figsize=(20,8))
     plt.figure(figsize=(15, 8))
 
     # print reward curve
@@ -20,11 +21,11 @@ def plot_results(result_memory):
     ax.grid()
 
     # print action count
-    ax2 = plt.subplot(gs[0, 1]) 
+    ax2 = plt.subplot(gs[0, 1])
     actions = list(map(lambda x: x.action, result_memory))
     print("action 0: ", actions.count(0), " actions 1: ", actions.count(1))
     labels, counts = np.unique(actions, return_counts=True)
-    ax2.bar(labels, counts, align='center')
+    ax2.bar(labels, counts, align="center")
     plt.gca().set_xticks(labels)
     ax2.set(
         xlabel="action",
@@ -33,17 +34,19 @@ def plot_results(result_memory):
     )
 
     # print where shorts and longs were taken
-    ax3 = plt.subplot(gs[1, :]) 
-    prices = list(map(lambda x: x.observation[len(x.observation)-1][0], result_memory))
+    ax3 = plt.subplot(gs[1, :])
+    prices = list(
+        map(lambda x: x.observation[len(x.observation) - 1][0], result_memory)
+    )
     ax3.plot(time, prices)
-    actions = list(map(lambda x: x.action,  result_memory))
+    actions = list(map(lambda x: x.action, result_memory))
     action = actions[0]
     longs = []
     long_prices = []
     shorts = []
     short_prices = []
     for i in range(1, len(actions)):
-        if(actions[i] != action):
+        if actions[i] != action:
             if actions[i] == Positions.Short and action == Positions.Long:
                 shorts.append(i)
                 short_prices.append(prices[i])
@@ -52,10 +55,12 @@ def plot_results(result_memory):
                 long_prices.append(prices[i])
             action = actions[i]
 
-    ax3.plot(longs, long_prices, 'o', color='green', markersize=3)
-    ax3.plot(shorts, short_prices, 'o', color='red', markersize=3)
+    ax3.plot(longs, long_prices, "o", color="green", markersize=3)
+    ax3.plot(shorts, short_prices, "o", color="red", markersize=3)
     ax3.set(
-        xlabel="time", ylabel="price", title="green = longs, red = shorts on the price of the asset"
+        xlabel="time",
+        ylabel="price",
+        title="green = longs, red = shorts on the price of the asset",
     )
 
     plt.show()
